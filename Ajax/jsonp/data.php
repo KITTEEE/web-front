8e2431483d3347fbc85bc23ff271b8a2bcd68148
Kite_Yiu <yhkkite@gmail.com>
2019-04-03 11:36:24 +0800

    normal commit

---
+++ Ajax/jsonp/data.php
@@ -0,0 +1,5 @@
+<?php
+    // echo 'var data={"username":"zhangsan","password":"123456}';
+    // echo 'foo({"username":"zhangsan","password":"123456"})';
+    echo '{"username":"zhangsan","password":"123456"}';
+ ?>
