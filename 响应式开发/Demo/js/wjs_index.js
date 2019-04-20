def26b84225576afc8896930be9843a2d87a3643
Kite_Yiu <yhkkite@gmail.com>
2019-04-20 17:24:36 +0800

    normal commit

--- 响应式开发/Demo/js/wjs_index.js
+++ 响应式开发/Demo/js/wjs_index.js
@@ -46,4 +46,23 @@ $(function () {
             carousel.carousel('next');
         }
     });
+
+    /*计算产品块导航项的原始宽度*/
+    var ul=$(".wjs_product .nav-tabs");
+    var lis=ul.find("li");
+    var totalWidth=0;//总宽度
+    lis.each(function(index,value){
+        totalWidth=totalWidth+$(value).innerWidth();
+        console.log($(value).innerWidth());
+        /*获取宽度的方法的说明：
+        * width():它只能得到当前元素的内容的宽度
+        * innerWidth():它能获取当前元素的内容的宽度+padding
+        * outerWidth():获取当前元素的内容的宽度+padding+border
+        * outerWidth(true):获取元素的内容的宽度+padding+border+margin*/
+    });
+    ul.width(totalWidth);
+    var myScroll = new IScroll('.tabs-parent',{
+        /*设置水平滑动，不允许垂直滑动*/
+        scrollX: true, scrollY: false
+    });
 })
